f94d1cfcc159b18bb6f947abaef6d3527c827e05
Gurudev Rana <Gurudev.Rana@mmc.com>
2021-09-15 13:32:53 +0530

    Test the build issue

--- TestApp/ConsoleApp1/Program.cs
+++ TestApp/ConsoleApp1/Program.cs
@@ -8,7 +8,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine("Hello World! dfdsfd");
             TestClass cl = new TestClass();
-            Console.WriteLine(cl.GetData());
+            Console.WriteLine(cl.GetData())
         }
     }
 }
